2e00a37d2626a4a8d2058047fc79f9150119ab86
Renson S. Placino <placinorenson1123@gmail.com>
2024-05-05 22:54:50 +0800

    Asking for Location Permission and Getting Client Current Location

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:capstone_project_carpool/authentication/login_screen.dart';
 import 'package:capstone_project_carpool/authentication/signup_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
+  {
+    if(valueOfPermission )
+      {
+        Permission.locationWhenInUse.request();
+      }
+  });
 
   runApp(const MyApp());
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:capstone_project_carpool/global/global_var.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget
@@ -18,6 +19,7 @@ class _HomePageState extends State<HomePage>
 {
   final Completer<GoogleMapController> googleMapCompleterController = Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
+  Position? currentPositionOfUser;
 
   void updateMapTheme(GoogleMapController controller)
   {
@@ -34,6 +36,18 @@ class _HomePageState extends State<HomePage>
   setGoogleMapStyle(String googleMapStyle, GoogleMapController controller)
   {
     controller.setMapStyle(googleMapStyle);
+  }
+
+  getCurrentLiveLocationOfUser() async
+  {
+    Position positionOfUser = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPositionOfUser = positionOfUser;
+
+    LatLng positionOfUserInLatLng = LatLng(currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
+
+    CameraPosition cameraPosition = CameraPosition(target: positionOfUserInLatLng, zoom: 15);
+    controllerGoogleMap!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
   }
 
   @override
@@ -55,6 +69,8 @@ class _HomePageState extends State<HomePage>
               // updateMapTheme(controllerGoogleMap!);
 
               googleMapCompleterController.complete(controllerGoogleMap);
+
+              getCurrentLiveLocationOfUser();
             },
           ),
         ],
